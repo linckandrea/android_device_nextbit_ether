2ff3d6248ed9427963d4f05f31d54dcf0ce1468f
linckandrea <linckandrea01@gmail.com>
2018-03-01 18:42:09 +0100

    Change SDclang version (4.0 is for oreo)
    Change-Id: I45680c8a98d4d5eca22f6aad59f5072edce1eb23

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -160,7 +160,7 @@ TARGET_RIL_VARIANT := caf
 TARGET_USE_SDCLANG := true
 ifneq ($(HOST_OS),darwin)
 SDCLANG := true
-SDCLANG_PATH := prebuilts/clang/linux-x86/host/SDclang-4.0/bin
+SDCLANG_PATH := prebuilts/clang/linux-x86/host/SDclang-3.8/bin
 SDCLANG_LTO_DEFS := device/qcom/common/sdllvm-lto-defs.mk
 SDCLANG_COMMON_FLAGS := -O3
 endif
